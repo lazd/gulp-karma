1360ed5c58e26455b1dadd5ac6f1e6c92d35ca0e
Larry Davis <lazdnet@gmail.com>
2014-04-17 19:34:43 -0700

    More refactoring

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 'use strict';
 
-var gutil = require('gulp-util');
 var extend = require('xtend');
 var path = require('path');
 var spawn = require('child_process').spawn;
@@ -10,151 +9,166 @@ var karmaParseConfig = require('karma/lib/config').parseConfig;
 
 var runner = require('karma').runner;
 
-var karmaPlugin = function(options) {
+var methods = {
+  start: function(cb, options) {
+
+    if (this._serverStarted) {
+      console.log('Karma server already started');
+      return;
+    }
+
+    var self = this;
+    this._serverStarted = true;
+
+    options = extend(this.options, options);
+
+    if (options.debug) {
+      console.log('Starting Karma server...');
+    }
+
+    // Start the server
+    // A child process is used because server.start() refuses to die unless you do a process.exit()
+    // Doing a process.exit() would muck with gulp, so we have to take other measures
+    // See https://github.com/karma-runner/karma/issues/734
+    var child = this.child = spawn(
+      'node',
+      [
+        path.join(__dirname, 'lib', 'background.js'),
+        JSON.stringify(options)
+      ],
+      {
+        stdio: [
+          'pipe',
+          'pipe',
+          'pipe'
+        ]
+      }
+    );
+
+    // Cleanup when the child process exits
+    child.on('exit', function(code) {
+      if (self.options.debug) {
+        console.log('Karma server ended');
+      }
+
+      if (typeof cb === 'function') {
+        cb(code);
+      }
+    });
+
+    child.stderr.on('data', function(data) {
+      if (!self._ignoreServerOutput) {
+        process.stderr.write(data);
+      }
+    });
+
+    child.stdout.on('data', function(data) {
+      if (!self._ignoreServerOutput) {
+        process.stdout.write(data);
+      }
+
+      var str = data.toString();
+
+      if (str.match(/Starting browser/)) {
+        // Store the number of browsers we're waiting to capture
+        self._browsersLeft++;
+      }
+
+      if (str.match(/Connected/)) {
+        // Do nothing unless we're waiting
+        // Otherwise, reconnects can result in negative _browsersLeft
+        if (!self._serverReady) {
+          // One less browser to wait for
+          self._browsersLeft--;
+
+          if (self._browsersLeft === 0) {
+            self._handleServerReady();
+          }
+          else {
+            if (self.options.debug) {
+              console.log('Waiting for '+self._browsersLeft+' browsers...');
+            }
+          }
+        }
+      }
+    });
+
+    return this;
+  },
+
+  stop: function() {
+    if (this.child) {
+      if (this.options.debug) {
+        console.log('Killing Karma server...');
+      }
+      this.child.kill();
+    }
+    else if (this.options.debug) {
+      console.log('Karma server not running');
+    }
+
+    return this;
+  },
+
+  once: function(cb, options) {
+    this.start(cb, extend(options, {
+      singleRun: true,
+      autoWatch: false // @todo might not be needed
+    }));
+
+    return this;
+  },
+
+  run: function(cb, options) {
+    if (!this._serverReady) {
+      this._runWhenServerReady = true;
+      return;
+    }
+
+    var self = this;
+
+    // Runner and server will the same output
+    // Stop the server's output from displaying
+    this._ignoreServerOutput = true;
+    runner.run(extend(this.options, options), function(code) {
+      self._ignoreServerOutput = false;
+
+      if (typeof cb === 'function') {
+        cb(code);
+      }
+    });
+
+    return this;
+  },
+
+  _handleServerReady: function() {
+    this._serverReady = true;
+
+    if (this._runWhenServerReady) {
+      this.run();
+      this._runWhenServerReady = false;
+    }
+  }
+};
+
+var karmaHelper = function(options) {
   options.configFile = path.resolve(options.configFile);
 
-  return {
-    _ignoreServerOutput: false, // On when running with runner
-    _browsers: 0,
-    _waitingForBrowsers: true,
-    _serverStarted: false,
-    _serverReady: false,
-    _runQueued: false,
-    _runIfQueued: function() {
-      if (this._runQueued) {
-        this.run();
-        this._runQueued = false;
-      }
-    },
+  return extend({}, methods, {
     options: options,
-    start: start,
-    stop: stop,
-    run: run,
-    once: once
-  };
+
+    // Disables server output
+    _ignoreServerOutput: false,
+
+    // The numbers of browsers remaining to be captures
+    _browsersLeft: 0,
+
+    // Whether a child process has been started for the server
+    _serverStarted: false,
+
+    // Whether to perform a run when the server is ready
+    _runWhenServerReady: false
+  });
 };
 
-function start(cb, options) {
-  var self = this;
-
-  if (this._serverStarted) {
-    gutil.log('Karma server already started');
-    return;
-  }
-
-  this._serverStarted = true;
-
-  gutil.log('Starting Karma server...');
-
-  options = extend(this.options, options);
-
-  // Start the server
-  // A child process is used because server.start() refuses to die unless you do a process.exit()
-  // See https://github.com/karma-runner/karma/issues/734
-  var child = this.child = spawn(
-    'node',
-    [
-      path.join(__dirname, 'lib', 'background.js'),
-      JSON.stringify(options)
-    ]
-  );
-
-  // Cleanup when the child process exits
-  child.on('exit', function(code) {
-    gutil.log('Karma server ended');
-
-    if (typeof cb === 'function') {
-      cb(code);
-    }
-  });
-
-  child.stderr.on('data', function(data) {
-    if (!self._ignoreServerOutput) {
-      process.stderr.write(data);
-    }
-  });
-
-  child.stdout.on('data', function(data) {
-    if (!self._ignoreServerOutput) {
-      process.stdout.write(data);
-    }
-
-    var str = data.toString();
-
-    if (str.match(/Karma .*? server started/)) {
-      gutil.log('Karma server ready');
-      self._serverReady = true;
-    }
-
-    if (str.match(/Starting browser/)) {
-      // Store the number of browsers we're waiting to capture
-      self._browsers++;
-      self._waitingForBrowsers = true;
-      gutil.log('Waiting for '+self._browsers+' browsers...');
-    }
-
-    if (str.match(/Connected/)) {
-      // Do nothing unless we're waiting
-      if (!self._waitingForBrowsers) {
-        return;
-      }
-
-      self._browsers--;
-      if (self._browsers === 0) {
-        self._waitingForBrowsers = false;
-        gutil.log('All browsers captured!');
-        self._runIfQueued();
-      }
-      else {
-        gutil.log('Waiting for '+self._browsers+' browsers...');
-      }
-    }
-  });
-
-  return this;
-}
-
-function stop() {
-  if (this.child) {
-    gutil.log('Killing Karma server');
-    this.child.kill();
-  }
-  else {
-    gutil.log('Karma server not running');
-  }
-
-  return this;
-};
-
-function once(cb, options) {
-  this.start(cb, extend(options, {
-    singleRun: true,
-    autoWatch: false // @todo might not be needed
-  }));
-
-  return this;
-};
-
-function run(cb, options) {
-  var self = this;
-
-  if (!this._serverReady) {
-    this._runQueued = true;
-    return;
-  }
-
-  this._ignoreServerOutput = true;
-  runner.run(extend(this.options, options), function(code) {
-    self._ignoreServerOutput = false;
-
-    gutil.log('Karma run completed '+(code === 0 ? 'successfully' : 'with error '+code));
-
-    if (typeof cb === 'function') {
-      cb(code);
-    }
-  });
-
-  return this;
-};
-
-module.exports = karmaPlugin;
+module.exports = karmaHelper;
